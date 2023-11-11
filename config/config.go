9e07015c835c8c6bd1f72ceccae3588a661e8459
arewabolu <bolu.arewa@gmail.com>
2023-11-11 17:12:07 +0100

    add methods to build easy config

--- config/config.go
+++ config/config.go
@@ -25,3 +25,27 @@ type AppConfig struct {
 func (a *AppConfig) GenUrl() string {
 	return fmt.Sprintf("https://www.flashscore.com/%s/%s/%s-%s/results/", a.Cfg.Sport, a.Cfg.Country, a.Cfg.League, a.Cfg.Season)
 }
+
+func NewAppConfig(cfg Config) *AppConfig {
+	return &AppConfig{Cfg: &cfg}
+}
+
+func NewConfig() *Config {
+	return &Config{}
+}
+
+func (c *Config) SetSport(sport string) {
+	c.Sport = sport
+}
+
+func (c *Config) SetCountry(country string) {
+	c.Country = country
+}
+
+func (c *Config) SetLeague(league string) {
+	c.League = league
+}
+
+func (c *Config) SetSeason(season string) {
+	c.Season = season
+}
